b89111011f7e711c2d67ba084b26559f2033aa50
helbadao <you@example.com>
2025-09-09 18:05:39 +0100

    nothing

---
+++ target/debug/deps/drawing-3b774cea2dda06ac.d
@@ -0,0 +1,6 @@
+/home/helbadao/drawing/target/debug/deps/drawing-3b774cea2dda06ac.d: src/main.rs src/geometrical_shapes.rs
+
+/home/helbadao/drawing/target/debug/deps/libdrawing-3b774cea2dda06ac.rmeta: src/main.rs src/geometrical_shapes.rs
+
+src/main.rs:
+src/geometrical_shapes.rs:
---
+++ target/debug/deps/drawing-7c8c8282dfde00d4.d
@@ -0,0 +1,6 @@
+/home/helbadao/drawing/target/debug/deps/drawing-7c8c8282dfde00d4.d: src/main.rs src/geometrical_shapes.rs
+
+/home/helbadao/drawing/target/debug/deps/libdrawing-7c8c8282dfde00d4.rmeta: src/main.rs src/geometrical_shapes.rs
+
+src/main.rs:
+src/geometrical_shapes.rs:
